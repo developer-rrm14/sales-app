e16ecf17e8052c07a72c4e73a6df8a45d4afb54b
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-07 01:29:53 -0300

    Domain Mode and Database Seed

---
+++ backend/src/main/java/com/rrm14/salesapp/entity/SaleEntity.java
@@ -0,0 +1,35 @@
+package com.rrm14.salesapp.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_sales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private SellerEntity seller;
+}
---
+++ backend/src/main/java/com/rrm14/salesapp/entity/SellerEntity.java
@@ -0,0 +1,31 @@
+package com.rrm14.salesapp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_sellers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SellerEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<SaleEntity> sales = new ArrayList<>();
+}
