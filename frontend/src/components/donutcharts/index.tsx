111b2cd73c67457ee014f7374a7df24032957541
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-09 02:01:17 -0300

    BarChart Integration

--- frontend/src/components/donutcharts/index.tsx
+++ frontend/src/components/donutcharts/index.tsx
@@ -5,15 +5,15 @@ import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { AMOUNT_BY_SELLER, BASE_URL } from "utils/requests";
 
-type ChartData = {
-  labels: string[];
-  series: number[];
-};
-
 //const mockData = {
 //  series: [477138, 499928, 444867, 220426, 473088],
 //  labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
 //};
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+};
 
 const options = {
   legend: {
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = "http://localhost:8080";
 export const AMOUNT_BY_SELLER = "/sales/amount-by-seller";
+export const SUCCESS_BY_SELLER = "/sales/success-by-seller";
