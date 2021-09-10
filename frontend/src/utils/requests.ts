896f0cff538f0a897994f05020c981510c24b207
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-10 11:35:03 -0300

    Env Variables

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
 export const AMOUNT_BY_SELLER = "/sales/amount-by-seller";
 export const SUCCESS_BY_SELLER = "/sales/success-by-seller";
 export const SALES_PAGINATION = "/sales?size=10&sort=date,desc&page=";
