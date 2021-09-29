c1332276c1b458f832fe41d564cf9a35aba5a48d
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-29 18:03:20 -0300

    Removing Env Variables

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = "https://rrm14-sales-app.herokuapp.com";
 export const AMOUNT_BY_SELLER = "/sales/amount-by-seller";
 export const SUCCESS_BY_SELLER = "/sales/success-by-seller";
 export const SALES_PAGINATION = "/sales?size=10&sort=date,desc&page=";
